c4f0b4624b37c3946bd8670127de44dd61b07897
dsalmela <dsalmela@gmail.com>
2015-03-18 09:43:06 -0500

    Update WordCountMapper.java

--- MR_Json/src/main/java/com/mrjson/hadoop/MapReduce/WordCountMapper.java
+++ MR_Json/src/main/java/com/mrjson/hadoop/MapReduce/WordCountMapper.java
@@ -1,34 +1,29 @@
 package com.mrjson.hadoop.MapReduce;
 
 import java.io.IOException;
-//import java.util.Formatter;
-//import java.util.Locale;
+
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-//import org.json;
 
 import com.cloudera.com.amazonaws.util.json.JSONException;
-//import com.cloudera.com.amazonaws.util.json.JSONException;
+
 import com.cloudera.com.amazonaws.util.json.JSONObject;
 public class WordCountMapper extends
         Mapper<Object, Text, Text, IntWritable> {
  
- //   private final IntWritable ONE = new IntWritable(1);
- //   private Text word = new Text();
+
 	 public void map(Object key, Text value, Context context)
 	            throws IOException, InterruptedException {
-	    		//StringBuilder sb = new StringBuilder();
-	    		//Formatter formatter = new Formatter(sb, Locale.US);
-	    		
+
 		// 		String[] fields = {"date", "type", "id", "user"};
 	            String formatted = value.toString().replaceAll("\n", " ");
 	            String out = "";
 	            try {
 	            	
 					JSONObject jObject = new JSONObject(formatted);
-					//d = jObject.getString("date");
+
 					out = String.format("%1$2s,%2$2s,%3$2s,%4$2s", jObject.getString("id"), jObject.getString("type"), jObject.getString("user"), jObject.getString("date")) ;
 				   
 				} catch (JSONException e) {
@@ -36,29 +31,11 @@ public class WordCountMapper extends
 					out = "error in json input" + e.toString();
 					e.printStackTrace();
 				}
-	            //String out = "";
-	            
 	            // emit the tuple and the original contents of the line
 	            
-	           // context.write(new Text(out), null);
+
 	            context.write(new Text(out), null);
 	       // }
 	    }
-    public void map_1(Object key, Text value, Context context)
-            throws IOException, InterruptedException {
-    		
-      //  String[] csv = value.toString().split(",");
-      //  for (String str : csv) {
-       //     word.set(str);
-       //     context.write(word, ONE);
-            // String[] fields = {"date", "type", "id", "user"};
-            String formatted = value.toString().replaceAll("\n", " ");
-            //String out = "";
-            
-            // emit the tuple and the original contents of the line
-            
-           // context.write(new Text(out), null);
-            context.write(new Text(formatted), null);
-       // }
-    }
+
 }
