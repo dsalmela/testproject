4f1cf2fe88d63e36c478d040babdacbf7bddc5fa
dsalmela@palominodb.com <dsalmela@palominodb.com>
2015-03-18 11:33:16 -0500

    restore changes to wcm

--- MR_Json/src/main/java/com/mrjson/hadoop/MapReduce/WordCountMapper.java
+++ MR_Json/src/main/java/com/mrjson/hadoop/MapReduce/WordCountMapper.java
@@ -1,29 +1,34 @@
 package com.mrjson.hadoop.MapReduce;
 
 import java.io.IOException;
-
+//import java.util.Formatter;
+//import java.util.Locale;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+//import org.json;
 
 import com.cloudera.com.amazonaws.util.json.JSONException;
-
+//import com.cloudera.com.amazonaws.util.json.JSONException;
 import com.cloudera.com.amazonaws.util.json.JSONObject;
 public class WordCountMapper extends
         Mapper<Object, Text, Text, IntWritable> {
  
-
+ //   private final IntWritable ONE = new IntWritable(1);
+ //   private Text word = new Text();
 	 public void map(Object key, Text value, Context context)
 	            throws IOException, InterruptedException {
-
+	    		//StringBuilder sb = new StringBuilder();
+	    		//Formatter formatter = new Formatter(sb, Locale.US);
+	    		
 		// 		String[] fields = {"date", "type", "id", "user"};
 	            String formatted = value.toString().replaceAll("\n", " ");
 	            String out = "";
 	            try {
 	            	
 					JSONObject jObject = new JSONObject(formatted);
-
+					//d = jObject.getString("date");
 					out = String.format("%1$2s,%2$2s,%3$2s,%4$2s", jObject.getString("id"), jObject.getString("type"), jObject.getString("user"), jObject.getString("date")) ;
 				   
 				} catch (JSONException e) {
